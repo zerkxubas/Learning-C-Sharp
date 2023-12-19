b4faa0f0e2f6cdbe9ba72ae043bb310f43134555
subash <zerkxubas@gmail.com>
2023-12-19 20:08:21 +0545

    Exercise Lab 1.1

---
+++ LAB1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taking Two Inputs
+            Console.WriteLine("Enter The First Number : ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The First Number : ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            
+            // Calculating the Sum & Printing Output.
+            Console.WriteLine("The Sum is {0}", firstNumber+secondNumber);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB1/lab1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Lab 1.1*/
+
+// Write a C# program to get numeric input in two variables and calculate the sum.
+using System;
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taking Two Inputs
+            Console.WriteLine("Enter The First Number : ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The First Number : ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            
+            // Calculating the Sum & Printing Output.
+            Console.WriteLine("The Sum is {0}", firstNumber+secondNumber);
+            Console.ReadKey();
+        }
+    }
+}
