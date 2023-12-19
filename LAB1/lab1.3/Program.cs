b8523e45246690c99ebcd7367f871726a037e877
subash <zerkxubas@gmail.com>
2023-12-19 21:02:01 +0545

    Exercise Lab 1.3 solved

---
+++ LAB1/lab1.3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Lab 1.3
+
+// Write a C# program to show the multiplication table of given number.
+
+using System;
+class Program{
+    static void Main(string[] args){
+        // Taking User Input For Desired Number
+        int userInput;
+        Console.WriteLine("Enter A Number To Get Multiplication Table : ");
+        userInput = Convert.ToInt32(Console.ReadLine());
+        // Now Calculating & Displaying the Multiplication Table
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{userInput} * {i} = {userInput*i}");            
+        }
+    }
+}
