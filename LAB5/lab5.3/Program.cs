025f963b75365bfab3dcfaaaa28b8ed70cf7fd12
Subash Sharma <zerkxubas@gmail.com>
2024-01-22 23:41:50 +0545

    Exercise Lab 5.3 solved

---
+++ LAB5/lab5.3/Program.cs
@@ -0,0 +1,30 @@
+﻿/** LAB5.3 */
+
+/*
+
+WAP in LINQ Syntax
+    a. First create List of strings with 4 string values
+    b. Get and show all string from List with LINQ
+    c. Filter and show the list containing the matching string text.
+
+*/
+using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> MyList = new List<string>{ "Pragun", "Lokey", "Kancha", "Ramu Dai"};
+            
+            var items = from something in MyList where something.Contains("a") select something;
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item}");
+            }
+            
+        }
+    }
+}
